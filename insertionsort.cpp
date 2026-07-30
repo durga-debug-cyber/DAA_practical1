ec8142d7eb81bf0c231707855188933e546b1710
durga-debug-cyber <aradhyula.durgaprasad130718@marwadiuniversity.ac.in>
2026-07-30 13:53:34 +0530

    Refactor insertionSort by removing comments
    Removed comments to streamline the insertion sort function.

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -1,27 +1,21 @@
 #include <iostream>
 #include <vector>
 
-// Function to perform insertion sort
 void insertionSort(std::vector<int>& arr) {
     int n = arr.size();
     
-    // Start from the second element (index 1)
     for (int i = 1; i < n; i++) {
-        int key = arr[i]; // The element to be positioned
+        int key = arr[i];
         int j = i - 1;
 
-        // Shift elements of arr[0..i-1] that are greater than key
-        // to one position ahead of their current position
         while (j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j];
             j--;
         }
-        // Place the key at its correct sorted position
         arr[j + 1] = key;
     }
 }
 
-// Utility function to print the array
 void printArray(const std::vector<int>& arr) {
     for (int num : arr) {
         std::cout << num << " ";
